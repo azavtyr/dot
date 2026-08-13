25e01131fea5722fa3ef09c41a6afd4e83f5b8a6
azavtyr <dmytro.m@tuta.io>
2026-08-13 02:07:12 +0200

    disable audio bell

--- wezterm/wezterm-mac.lua
+++ wezterm/wezterm-mac.lua
@@ -4,9 +4,10 @@ return {
   default_prog = {'/opt/homebrew/bin/bash','--login'}, 
   color_scheme = 'Gruvbox Material (Gogh)',
   font = wezterm.font('UbuntuMono Nerd Font'),
-  font_size = 30,
+  font_size = 28,
   term = "xterm-256color",
   animation_fps = 60,
   max_fps = 60,
   hide_tab_bar_if_only_one_tab = true,
+  audible_bell = "Disabled",
 }
